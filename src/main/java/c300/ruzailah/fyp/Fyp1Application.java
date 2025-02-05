59442b6b9ddc8d3f724cdf5036d30ad95f232328
Nur Ruzaliah <22040862@myrp.edu.sg>
2025-02-05 18:15:29 +0800

    added port

--- src/main/java/c300/ruzailah/fyp/Fyp1Application.java
+++ src/main/java/c300/ruzailah/fyp/Fyp1Application.java
@@ -1,11 +1,17 @@
 package c300.ruzailah.fyp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class Fyp1Application {
 
+	@Value("${PORT:8080}")
+	private String port;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Fyp1Application.class, args);
 	}
