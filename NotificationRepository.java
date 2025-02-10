dea50aac011fe6ff6e18a5c557ab0154d51927df
22040862-NurRuzailah <134899390+22040862-NurRuzailah@users.noreply.github.com>
2025-02-10 14:59:58 +0800

    Delete NotificationRepository.java
    wrong directory

--- NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package c300.ruzailah.fyp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    
-}
