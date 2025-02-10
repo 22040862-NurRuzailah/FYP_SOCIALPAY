08230e8f2d7f46a7c1ff4a651d524ed8c7e0bc2e
22040862-NurRuzailah <134899390+22040862-NurRuzailah@users.noreply.github.com>
2025-02-10 14:58:55 +0800

    Delete FriendRequestRepository.java
    wrong directory

--- FriendRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package c300.ruzailah.fyp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
-    
-}
